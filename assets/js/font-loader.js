291869ec9d19f9cbad09cee1df0e5152b6d514b1
jadchaar <jad.chaar@gmail.com>
2017-08-07 23:36:12 +0300

    initial upload

---
+++ assets/js/font-loader.js
@@ -0,0 +1,13 @@
+WebFontConfig = {
+  google: {
+    families: ["Open Sans:400,700", "Space Mono:700"]
+  }
+};
+
+(function(d) {
+  var wf = d.createElement('script'),
+    s = d.scripts[0];
+  wf.src = 'https://cdnjs.cloudflare.com/ajax/libs/webfont/1.6.28/webfontloader.js';
+  wf.async = true;
+  s.parentNode.insertBefore(wf, s);
+})(document);
